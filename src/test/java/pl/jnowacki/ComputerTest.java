1a3c2c630f61df88383bda5309028e067e73f57d
Jedrzej Nowacki <contact@jnowacki.pl>
2019-12-08 15:27:52 +0100

    Keyboard and screen test

--- src/test/java/pl/jnowacki/ComputerTest.java
+++ src/test/java/pl/jnowacki/ComputerTest.java
@@ -1,5 +1,6 @@
 package pl.jnowacki;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -9,7 +10,29 @@ import pl.jnowacki.model.Keyboard;
 import pl.jnowacki.model.Printer;
 import pl.jnowacki.model.Screen;
 
+import static org.mockito.Mockito.*;
+
 @RunWith(MockitoJUnitRunner.class)
 public class ComputerTest {
 
+    @Mock
+    private Printer printer;
+
+    @Mock
+    private Screen screen;
+
+    @Mock
+    private Keyboard keyboard;
+
+    @InjectMocks
+    private Computer computer;
+
+    @Test
+    public void testMock() {
+        when(keyboard.readValue()).thenReturn("valuFromKeyboard");
+
+        computer.readValueFromKeyboardAndDisplayInput();
+
+        verify(screen, times(1)).showMessage("valuFromKeyboard");
+    }
 }
